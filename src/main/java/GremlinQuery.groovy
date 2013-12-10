b2cd8de5d9923c97dc6fd62d746c9dea416c35f3
Paola Accioly <prga@cin.ufpe.br>
2013-12-09 22:15:52 -0300

    Ajeitando estrutura

--- src/main/java/GremlinQuery.groovy
+++ src/main/java/GremlinQuery.groovy
@@ -1,32 +1,63 @@
 import org.codehaus.groovy.antlr.Main;
-
-import com.tinkerpop.blueprints.Graph
-import com.tinkerpop.blueprints.Vertex
+import com.tinkerpop.blueprints.impls.tg.*
 import com.tinkerpop.blueprints.*
+import com.tinkerpop.blueprints.impls.neo4j.Neo4jGraph
 import com.tinkerpop.gremlin.groovy.Gremlin
+import com.tinkerpop.gremlin.groovy.GremlinGroovyPipeline
+import com.tinkerpop.gremlin.pipes.transform.PropertyMapPipe;
 import com.tinkerpop.gremlin.*
+import com.tinkerpop.pipes.util.StartPipe;
 
-import com.tinkerpop.blueprints.impls.tg.TinkerGraph
-import com.tinkerpop.blueprints.impls.tg.TinkerGraphFactory
+import org.neo4j.management.impl.*
 
 
 class GremlinQuery {
+	
+	ArrayList<Mergecommit> mergeCommitslist = new ArrayList<Mergecommit>()
+	
 	static {
 		Gremlin.load()
 	  }
-	public List exampleMethod() {
-		Graph g = TinkerGraphFactory.createTinkerGraph()
+	
+	public void getMergeCommits() {
 		
-		//new Neo4jGraph("/Users/paolaaccioly/Desktop/graph.db") 
-		def results = []
-		 g.v(1).out('knows').fill(results)
-		return results
+		def g = new Neo4jGraph("/Users/paolaaccioly/Documents/Doutorado/gitminer/graph.db")
+		
+		def queryCommits = g.V.map.filter{it._type == "COMMIT" & it.isMerge == true}.sort{it.date}
+
+		for(commits in queryCommits){
+			
+			//queryCommits.getProperties().
+			//mergeCommitslist.add()
+			
+		}
+		
 	  }
 	
+	public void getParents(){
+		
+	}
+	
+	public void getParentsCommonAncestor(){
+		
+	}
+	
 	public static void main (String[] args){
-		def g = new GremlinQuery();
-		def results = g.exampleMethod();
-		println(results)
+		//def g = new GremlinQuery();
+		
+		Gremlin.load()
+		
+		def g = new Neo4jGraph("/Users/paolaaccioly/Documents/Doutorado/gitminer/graph.db")
+		def results = g.V.map.filter{it._type == "COMMIT" & it.isMerge == true}.sort{it.date}
+		
+			/*for ( HashMap<StartPipe, PropertyMapPipe> m in results) {
+				
+		def sha = m.keySet().getAt("hash")
+				println(sha)
+			}*/
+			
+			
+		
 	}
 	
 	
---
+++ src/main/java/Mergecommit.groovy
@@ -0,0 +1,11 @@
+
+class Mergecommit {
+	
+	String sha
+	String parent1
+	String parent2
+	String parentsCommonAncestor
+	
+	
+
+}
