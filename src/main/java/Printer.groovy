67667284b54628ba700ac40c4bca5f0c62bcb68b
Paola Accioly <prga@cin.ufpe.br>
2013-12-11 13:33:02 -0300

    improving .csv fileof feature printer

--- src/main/java/Printer.groovy
+++ src/main/java/Printer.groovy
@@ -6,6 +6,11 @@ class Printer {
 	public void writeCSV(ArrayList<MergeCommit> listMC){
 		
 		def out = new File('commits.csv')
+		
+		def firstRow = ["Merge commit", "Parent 1", "Parent 2"]
+		out.append firstRow.join(',')
+		out.append '\n'
+		
 		listMC.each {
 			def row = [it.sha, it.parent1, it.parent2]
 			out.append row.join(',')
