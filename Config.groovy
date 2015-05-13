49775723d21ee096070e77c8d2ecea752e40ad55
thaisabr <tabr@cin.ufpe.br>
2015-05-13 16:35:28 -0300

    Configuration file to define bd path and search parameters

---
+++ Config.groovy
@@ -0,0 +1,8 @@
+//Path of graph database (to change)
+bd.path = "${System.getProperty("user.home")}${File.separator}Documents${File.separator}GraphBDs${File.separator}rgms${File.separator}graph.db"
+
+//Mining commits by message
+mining.keywords = ["bookChapter", "bookchapter", "BookChapter", "book chapter", "Book Chapter"]
+
+//Mining commits by file
+mining.files = ["BookChapter.groovy", "BookChapterController.groovy"]
