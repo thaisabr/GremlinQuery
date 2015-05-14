b940e8d8550f03ea23bf9c87dc7f6ffbbfab9789
thaisabr <tabr@cin.ufpe.br>
2015-05-14 09:42:18 -0300

    Usage of configuration file to define bd path and search parameters

--- src/search/App.groovy
+++ src/search/App.groovy
@@ -3,14 +3,18 @@ package search
 class App {
 
 	public static void main (String[] args){
-        CommitsQuery cq = new CommitsQuery("C:\\Users\\Thaís\\Documents\\Repos para minerar\\tmp\\graph.db")
-        def commits = cq.searchByComment(["bookChapter", "bookchapter", "BookChapter", "book chapter", "Book Chapter"])
-        commits.each{
-            println it.properties
-        }
-        println "Result size: ${commits.size()}"
+        def config = new ConfigSlurper().parse(new File("Config.groovy").toURI().toURL())
+        CommitsQuery cq = new CommitsQuery(config.bd.path)
 
-        Printer.writeCSV(commits)
+        def commitsByComments = cq.searchByComment(config.mining.keywords)
+        println "Result size: ${commitsByComments.size()}"
+
+        def commitsByFile = cq.searchByFile(config.mining.files)
+        println "Result by files size: ${commitsByFile.size()}"
+
+        def finalResult = (commitsByComments + commitsByFile) as Set
+        println "Final result size: ${commitsByFile.size()}"
+        Printer.writeCSV(finalResult as List)
         println('Finished!')
 
 	}
