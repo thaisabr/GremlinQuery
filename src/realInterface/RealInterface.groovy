d954683a12b8965abaed208291b77aa538f0c470
thaisabr <tabr@cin.ufpe.br>
2015-06-17 11:00:42 -0300

    Associating real interfaces and commits.

--- src/realInterface/RealInterface.groovy
+++ src/realInterface/RealInterface.groovy
@@ -1,14 +1,17 @@
 package realInterface
 
+import search.Commit
 
 class RealInterface {
 
+    Set<Commit> commits
     Set files //changed classes
     Set methods //static and non-static changed methods
     Set staticFields //changed static fields
     Set fields //changed fields
 
     public RealInterface(){
+        this.commits = [] as Set
         this.files = [] as Set
         this.methods = [] as Set
         this.staticFields = [] as Set
