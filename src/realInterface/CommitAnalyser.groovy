b02d52b3e646bdf4d63f7c2193fd1703c8160588
thaisabr <tabr@cin.ufpe.br>
2015-06-17 11:03:56 -0300

    Refactoring (class renaming and exclusion of methods)

---
+++ src/realInterface/CommitAnalyser.groovy
@@ -0,0 +1,36 @@
+package realInterface
+
+import org.eclipse.jgit.lib.Repository
+import search.Commit
+import search.CommitManager
+
+class CommitAnalyser {
+
+    Repository repository
+    CommitManager manager
+
+    public CommitAnalyser(CommitManager manager){
+        this.manager = manager
+    }
+
+    RealInterface computeTaskInterfaceForCommit(String sha){
+        Commit commit = manager?.searchBySha(sha)
+        return new RealInterface(commits:[commit] as Set, files:commit.files.sort() as Set)
+    }
+
+    RealInterface computeTaskInterface(){
+        List<Commit> commits = manager?.search()
+        return new RealInterface(commits:commits as Set, files:(commits*.files.flatten().unique().sort()) as Set)
+    }
+
+    RealInterface computeTaskInterface(String taskId){
+        List<Commit> commits = manager?.searchByComment([taskId])
+        return new RealInterface(commits:commits as Set, files:(commits*.files.flatten().unique().sort()) as Set)
+    }
+
+    RealInterface computeTaskInterface(List words, List files){
+        List<Commit> commits = manager?.search(words, files)
+        return new RealInterface(commits:commits as Set, files:(commits*.files.flatten().unique().sort()) as Set)
+    }
+
+}
